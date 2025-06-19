73abbf37e44bb787269bd10229de49b09bfcf615
Diego Carmona <diegoncarmona@gmail.com>
2025-06-19 08:18:41 -0300

    initial commit

---
+++ static-page-react/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img className="header-logo" src="src/assets/react.svg" alt="React logo" />
+                <span className="header-title">ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
