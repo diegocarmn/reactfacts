d42eff9a5ef132a2d79b7420ab8ef6b99bd192ed
Diego Carmona <diegoncarmona@gmail.com>
2025-06-20 01:26:34 -0300

    fix navbar logo path

--- static-page-react/src/components/Navbar.jsx
+++ static-page-react/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-export default function Navbar() {
+import logo from '../assets/react.svg';
+
+function Navbar() {
     return (
         <header>
             <nav>
-                <img className="header-logo" src="src/assets/react.svg" alt="React logo" />
+                <img className="header-logo" src={logo} alt="React logo" />
                 <span className="header-title">ReactFacts</span>
             </nav>
         </header>
     )
 }
+
+export default Navbar; 
