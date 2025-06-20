b238dea01e971f5f31fbc950da3769a3c816733b
Diego Carmona <diegoncarmona@gmail.com>
2025-06-20 00:39:52 -0300

    added background image and change files

--- static-page-react/src/App.jsx
+++ static-page-react/src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from './components/Navbar'
-import Main from './components/Main'
+import MainContent from './components/MainContent'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Main />
+      <MainContent />
     </>
   )
 }
--- static-page-react/src/components/MainContent.jsx
+++ static-page-react/src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-export default function Main() {
+export default function MainContent() {
     return (
         <main>
             <h1>Fun facts about React</h1>
